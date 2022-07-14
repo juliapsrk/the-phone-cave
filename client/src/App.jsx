c7307f11a5c6a748751ee4a5c5fd573e4378f537
Julia <juliapsrk@gmail.com>
2022-07-14 17:32:27 +0200

    initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import Navbar from './components/Navbar';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
